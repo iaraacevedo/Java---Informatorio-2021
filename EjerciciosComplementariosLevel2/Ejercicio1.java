3a338497d647a761b16e9442008f8fd1eea0848a
iaraacevedo <iaracevedo3@gmail.com>
2021-10-14 22:53:21 -0300

    mas ejercicios

---
+++ EjerciciosComplementariosLevel2/Ejercicio1.java
@@ -0,0 +1,10 @@
+public class Ejercicio21 {
+    public static void main(String[] args){
+       String [] ciudades = {"#1 - San Ignacio", "#2 - Oberá", "#3 - Corrientes"};
+
+        for (String i : ciudades) {
+            System.out.println(i);
+        }
+    }
+}
+
